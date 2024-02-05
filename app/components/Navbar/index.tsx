1d9ceb31d53fb0192c4a16da65564f11040db3d3
paanSinghCoder <gaurav3017@gmail.com>
2024-02-05 17:54:53 +0530

    feat: implement recording feature

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 				<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 					<div className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
 						<div className="ml-4 flex flex-row text-center items-center justify-center gap-6">
-							<span className="opacity-70 hover:opacity-100 cursor-pointer">Settings</span>
+							<span className="opacity-70 hover:opacity-100 cursor-pointer">Github</span>
 						</div>
 					</div>
 				</div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,77 @@ import Navbar from './components/Navbar'
 import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
-	const [isPermissionGranted, setPermissionGranted] = useState<any>(true)
-	const videoRef = useRef(null)
+	const [isPermissionGranted, setPermissionGranted] = useState<boolean>(true)
+	const [isRecording, setIsRecording] = useState<boolean>(false)
+	const [stream, setStream] = useState<any>(null)
+	const [videoChunks, setVideoChunks] = useState<any>(null)
+	const [video, setVideo] = useState<any>(null)
+	const videoRef = useRef<any>(null)
+
+	// Count timer
+	const [isActive, setIsActive] = useState(false)
+	const [seconds, setSeconds] = useState(0)
 
 	useEffect(() => {
-		navigator.mediaDevices.getUserMedia({ audio: false, video: true }).then(
-			mediaStream => {
-				setPermissionGranted(true)
-				let video: any = videoRef.current
-				video.srcObject = mediaStream
-				video.addEventListener('loadedmetadata', () => {
-					video.play()
-				})
-			},
-			e => {
-				setPermissionGranted(false)
-			}
-		)
+		let timer: any = null
+		if (isActive) {
+			timer = setInterval(() => {
+				setSeconds(seconds => seconds + 1)
+			}, 1000)
+		}
+		return () => {
+			clearInterval(timer)
+		}
+	})
+
+	useEffect(() => {
+		if ('MediaRecorder' in window) {
+			navigator.mediaDevices.getUserMedia({ audio: false, video: true }).then(
+				mediaStream => {
+					setPermissionGranted(true)
+					let video: any = videoRef.current
+					video.srcObject = mediaStream
+					setStream(mediaStream)
+					video.addEventListener('loadedmetadata', () => {
+						video.play()
+					})
+				},
+				e => {
+					setPermissionGranted(false)
+				}
+			)
+		} else {
+			alert('Not supported.')
+		}
 	}, [])
+
+	const startRecord = () => {
+		setIsRecording(true)
+		const media = new MediaRecorder(stream, { mimeType: 'video/webm' })
+		videoRef.current = media
+		videoRef.current.start()
+
+		let videoRecChunks: any = []
+
+		videoRef.current.ondataavailable = (event: any) => {
+			if (typeof event.data === 'undefined') return
+			if (event.data.size === 0) return
+			videoRecChunks.push(event.data)
+		}
+
+		setVideoChunks(videoRecChunks)
+	}
+
+	const stopRecord = () => {
+		setIsRecording(false)
+		videoRef.current.stop()
+		videoRef.current.onstop = () => {
+			const videoBlob = new Blob(videoChunks, { type: 'video/webm' })
+			const videoURL = URL.createObjectURL(videoBlob)
+			setVideo(videoURL)
+			console.log(videoURL)
+		}
+	}
 
 	return (
 		<main className="w-screen h-screen">
@@ -28,19 +81,61 @@ export default function Home() {
 				<Navbar />
 			</div>
 			<div className="flex items-center justify-center pt-12">
-				<div className="flex flex-col items-center justify-center">
-					{!isPermissionGranted && (
-						<div className="text-red-700 font-bold text-xl">
-							Please grant audio / video permission and reload.
+				{!video ? (
+					<div className="flex flex-col items-center justify-center">
+						{!isPermissionGranted && (
+							<div className="text-red-700 font-bold text-xl">
+								Please grant audio / video permission and reload.
+							</div>
+						)}
+						<div className="">
+							<video className="h-full w-[890px] rounded-lg" ref={videoRef}></video>
 						</div>
-					)}
-					<div className="">
-						<video className="h-full w-[890px] rounded-lg" ref={videoRef}></video>
+						<div className="border rounded-3xl px-8 py-2 mt-10 flex items-center justify-center">
+							{isRecording ? (
+								<>
+									<div className="pr-4 opacity-60">Recording</div>
+									<img
+										onClick={() => stopRecord()}
+										src="/stop.png"
+										alt="stop"
+										className="h-9 w-auto cursor-pointer"
+									/>
+									<div className="pl-4 opacity-60">00.00.{seconds}</div>
+								</>
+							) : (
+								<img
+									onClick={() => {
+										startRecord()
+										setIsActive(true)
+									}}
+									src="/record.png"
+									alt="record"
+									className="h-12 w-auto cursor-pointer"
+								/>
+							)}
+						</div>
 					</div>
-					<div className="border rounded-3xl px-16 py-2 mt-10">
-						<img src="/record.png" alt="record" className="h-12 w-auto cursor-pointer" />
+				) : (
+					<div className="border rounded-md p-12 w-[890px] h-auto">
+						<div className="text-xl font-semibold mb-4">Here is your awsome video...</div>
+						<video src={video} className="h-auto w-full" controls></video>
+						<div className="flex gap-4  items-center justify-center w-full p-7">
+							<button
+								className="border rounded-md shadow px-4 py-2"
+								onClick={() => {
+									// setPermissionGranted(true)
+									// setVideo(null)
+									window.location.reload()
+								}}>
+								Record another video
+							</button>
+							<a className="border rounded-md shadow px-4 py-2 font-bold" download href={video}>
+								Download
+							</a>
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</main>
 	)
